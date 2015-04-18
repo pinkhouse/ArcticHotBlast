f4b4d854634b3a77c51a3a828d5445dddb6ce0df
justBORYS <bodzio3573@gmail.com>
2015-04-18 17:00:52 +0200

    added view file

---
+++ ArcticHotBlast/View.cpp
@@ -0,0 +1,36 @@
+#include "View.h"
+#include <iostream>
+
+View::View()
+{
+	scene = 0;
+}
+
+
+View::~View()
+{
+}
+
+void View::update()
+{
+	AssetLibrary::instance()->mainWindow->setView(*AssetLibrary::instance()->mainCamera);
+	CollidersDB::instance()->leftBorder->update(sf::Vector2f(AssetLibrary::instance()->mainCamera->getCenter().x
+		- AssetLibrary::instance()->mainCamera->getSize().x / 2 - 32, 0.0f));
+
+	CollidersDB::instance()->ground->update(sf::Vector2f(CollidersDB::instance()->leftBorder->getPosition().x + 32.0f,
+		AssetLibrary::instance()->mainWindow->getSize().y-32));
+	
+
+	float moveBorder = AssetLibrary::instance()->mainCamera->getCenter().x + 100;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		
+		if (CollidersDB::instance()->player->getPosition().x > moveBorder)		
+		{
+			AssetLibrary::instance()->mainCamera->move( CollidersDB::instance()->player->getPosition().x - moveBorder, 0);
+		}
+		
+	}
+
+}
